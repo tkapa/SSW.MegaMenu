7a4948f1eccd2769ce1c3950919808817258ce96
TiSeb <boissiere.seb@gmail.com>
2020-07-03 09:49:40 +1000

    Update menu

--- src/lib/menu/menu.js
+++ src/lib/menu/menu.js
@@ -7,63 +7,73 @@ import cs from 'classnames';
 import axios from 'axios';
 
 const searchUrl = `https://www.google.com.au/search?q=site:ssw.com.au%20`;
-
-var menuModel = require('../data/menu.json');
-//const Menu = ({ onClickToggle, prefix }) => {
-
-
-    class Menu extends React.Component {
-        //const DesktopMenu = ({prefix}) => {
-        constructor(props) {
-          super(props);
-          this.state = { menuModel: menuModel };
-        }
-      
-        loadMenuModel() {
-        if (this.state.menuModel=== menuModel){
-          let currentComponent = this;
-          axios.get('https://SSWConsulting.github.io/SSW.Website.Menu.json/menu.json')
-            .then(function (response) {
-              currentComponent.setState({ menuModel: response.data });
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-        }
-        }
-       componentDidMount(){
-            this.loadMenuModel();
-        }
-    menu_Search(search){
+class Menu extends React.Component {
+    menu_Search(search) {
         if (window) {
             window.open(searchUrl + search);
         }
     }
 
-    handleKeyDownOnMenuSearchInput(event){
+    handleKeyDownOnMenuSearchInput(event) {
         if (event.key === 'Enter') {
             this.menu_Search(event.target.value);
         }
     }
 
-    render(){
-    return (
-       // this.state.menuModel &&
-        <div className={styles.MegaMenu}>
-            <div className={styles.menuContent}>
-                <div className={cs(styles.menuMobile, styles.visibleXs, styles.visibleSm)}>
-                    <div className={styles.sbToggleLeft} onClick={() => this.props.onClickToggle()} >
-                        <FontAwesomeIcon icon={faBars} />
+
+
+    render() {
+        const { menuModel } = this.props;
+        return (
+            // this.state.menuModel &&
+            <div className={styles.MegaMenu}>
+                <div className={styles.menuContent}>
+                    <div className={cs(styles.menuMobile, styles.visibleXs, styles.visibleSm)}>
+                        <div className={styles.sbToggleLeft} onClick={() => this.props.onClickToggle()} >
+                            <FontAwesomeIcon icon={faBars} />
+                        </div>
+                    </div>
+                    <DesktopMenu prefix={this.props.prefix} menuModel={menuModel}></DesktopMenu>
+                    <div className={styles.menuSearch}>
+                        <input type="text" className={styles.searchBox} onKeyDown={(event) => this.handleKeyDownOnMenuSearchInput(event)} />
                     </div>
                 </div>
-                <DesktopMenu prefix={this.props.prefix} menuModel={this.state.menuModel}></DesktopMenu>
-                <div className={styles.menuSearch}>
-                    <input type="text" className={styles.searchBox} onKeyDown={(event) => this.handleKeyDownOnMenuSearchInput(event)} />
-                </div>
             </div>
-        </div>
-    )
+        )
+
+    }
 }
+class Wrapper extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuModel: require('../data/menu.json'),
+            menuLoaded: false
+        };
+    }
+
+    componentDidMount() {
+        let currentComponent = this;
+        axios.get('https://SSWConsulting.github.io/SSW.Website.Menu.json/menu.json')
+            .then(function (response) {
+                currentComponent.setState({ menuModel: response.data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        const { menuModel, menuLoaded } = this.state;
+
+        return (
+            <Menu
+                menuModel={menuModel}
+                menuLoaded={menuLoaded}
+                {...this.props}
+            />
+        );
+    }
 }
 
-export default Menu;
+export default Wrapper;
