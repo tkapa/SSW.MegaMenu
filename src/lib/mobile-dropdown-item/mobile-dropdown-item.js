905b6d7c2a4fd73fa363748fd4030d11101a16e5
Greg Harris <GregHarris@ssw.com.au>
2020-11-12 14:14:27 +1100

    Change menu links on sswcomau azure staging

--- src/lib/mobile-dropdown-item/mobile-dropdown-item.js
+++ src/lib/mobile-dropdown-item/mobile-dropdown-item.js
@@ -5,7 +5,7 @@ const MobileDropdownItem = ({ item, index }) => {
     return (
         <>
             <li key={index} className={styles.level1} >
-                <a href={item.navigateUrl} className={styles.ignore}>
+                <a href={item.navigateUrl ? window.location.host.indexOf('sswcomau-stage') > -1 ? item.navigateUrl.replace("www.ssw.com.au", window.location.host) : item.navigateUrl : null} className={styles.ignore}>
                     {item.text}
                 </a>
             </li>
