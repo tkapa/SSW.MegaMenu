9e936e3060ede258fc15217ce8a3110cfba5a95a
Brady Stroud <bradystroud@me.com>
2021-05-14 12:18:59 +1000

    📦  Update last packages :)

--- src/lib/menu-panel/menu-panel.js
+++ src/lib/menu-panel/menu-panel.js
@@ -13,10 +13,11 @@ const MenuPanel = ({ item, prefix }) => {
         }
         return '';
     }
+    console.error(require("../images/" + item.groupImageUrl).default);
     return (
         <>
             <div className={styles.MenuImg}>
-                <img src={getRootUrl() + require("../images/" + item.groupImageUrl)} alt={item.text} loading="eager" />
+                <img src={getRootUrl() + require("../images/" + item.groupImageUrl).default} alt={item.text} loading="eager" />
             </div>
             <Dropdown items={item.children}></Dropdown>
         </>
